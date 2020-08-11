bc525da53e14d491ede8607d58a1fdcef8597034
rafael Marques <markes.rafa@hotmail.com>
2020-08-11 20:39:08 -0300

    criamos a classe select

---
+++ class/Sql.php
@@ -0,0 +1,50 @@
+<?php  
+
+class Sql extends PDO {
+
+	private $conn;
+
+
+	public function __construct(){ // FUNCAO QUE CONECTA NO BANCO.
+
+		$this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "");
+
+	}
+
+	private function setParams($stantement, $parameters  = array())
+	{
+		foreach ($parameters  as $key => $value) {
+			
+			$this->setParam($stantement, $key, $value);
+		}
+	}
+
+	private function setParam($stantement, $key, $value)
+	{
+
+		$stantement->bindParam($key, $value);
+
+	}
+
+	public function query($RawQuery, $params = array()){
+
+		$stmt = $this->conn->prepare("$RawQuery");
+
+		$this->setParams($stmt, $params);
+
+		$stmt->execute();
+
+		return $stmt;
+
+	}
+
+	public function select($RawQuery, $params = array()):array
+	{
+		$stmt = $this->query($RawQuery, $params);
+
+		return $stmt->fetchAll(PDO::FETCH_ASSOC);
+	}
+
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,30 @@
+<?php  
+
+
+
+
+
+
+
+
+
+
+/*
+require_once("config.php");
+
+$root = new Usuario(); // SELECT DE UM ID ESPECIFICO.
+
+$root->loadById(8);
+
+echo $root;
+
+*/
+/*require_once("config.php");
+
+$sql = new Sql();
+
+$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+
+echo json_encode($usuarios); */ // SELECT SIMPLES DE TODOS OS DADOS.
+
+?>
